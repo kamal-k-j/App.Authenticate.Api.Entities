44ad22df1afd5941158a6cfbbd9e65b0fba982b5
Kamal Jassal <kamal.jassal@onthebeach.co.uk>
2020-01-25 12:01:45 +0000

    Add extra fields to User class
    The following properties have been added:
    
    - PasswordHash
    - HashSalt
    - DateOfBirth
    - PhoneNumberCountry
    - PhoneNumber
    
    Also, the minor version of the nuget package has been increased.

--- App.Authenticate.Api.Entities/Response/User.cs
+++ App.Authenticate.Api.Entities/Response/User.cs
@@ -1,11 +1,27 @@
-﻿namespace App.Authenticate.Api.Entities.Response
+﻿using System;
+
+namespace App.Authenticate.Api.Entities.Response
 {
     public class User
     {
         public int Id { get; set; }
+
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
+
         public string Email { get; set; }
+
         public string Token { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string HashSalt { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string PhoneNumberCountry { get; set; }
+
+        public string PhoneNumber { get; set; }
     }
 }
